90ca153a3b9ef3f1e2191779d275813213d392e3
Angel Vasilev <angavasilev@gmail.com>
2022-12-21 07:34:31 +0200

    add login and registration html templates

--- system/client/client/src/app/user/register/register.component.ts
+++ system/client/client/src/app/user/register/register.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  hide: boolean = true;
 
+  toggleHide(event: Event) {
+    event.preventDefault();
+    this.hide = !this.hide;
+  }
 }
