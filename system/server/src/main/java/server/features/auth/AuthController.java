f8c3fea47774e3a822860d22edec10cfc477bba5
Angel Vasilev <angavasilev@gmail.com>
2023-01-02 22:05:01 +0200

    start work on schedule

--- system/server/src/main/java/server/features/auth/AuthController.java
+++ system/server/src/main/java/server/features/auth/AuthController.java
@@ -5,14 +5,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import server.config.exceptions.models.EntityNotFoundException;
 import server.features.auth.model.LoginRequest;
+import server.features.auth.model.LoginResponse;
 import server.features.auth.model.RegistrationRequest;
 import server.utils.TokenService;
+
+import static server.constants.ErrorMessages.ENTITY_NOT_FOUND;
 
 @RestController
 @RequestMapping("/auth")
@@ -28,16 +33,29 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
         var authentication = this.authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
-        return ResponseEntity.ok(this.tokenService.generateToken(authentication, loginRequest.getPersist()));
+        return ResponseEntity.ok(this.getLoginResponse(authentication, loginRequest.getPersist()));
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> register(@Valid @RequestBody RegistrationRequest registrationRequest) {
-        var userId = this.authService.register(registrationRequest);
-        return new ResponseEntity<>(userId,HttpStatus.CREATED);
+        this.authService.register(registrationRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    private LoginResponse getLoginResponse(Authentication authentication, boolean persist) {
+        var role = authentication.getAuthorities()
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new EntityNotFoundException(ENTITY_NOT_FOUND)).toString();
+
+        return new LoginResponse(
+                this.tokenService.generateToken(authentication, persist),
+                role,
+                authentication.getName()
+        );
     }
 }
