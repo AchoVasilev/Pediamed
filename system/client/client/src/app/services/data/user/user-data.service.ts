a82b6d4d03107f14bdc1770f639ed6744470b825
Angel Vasilev <angavasilev@gmail.com>
2023-01-13 06:53:26 +0200

    work on data service

---
+++ system/client/client/src/app/services/data/user/user-data.service.ts
@@ -0,0 +1,36 @@
+import { UserModel } from './userModel';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserDataService {
+  private user: UserModel = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    role: '',
+  };
+
+  private readonly currentUserSource = new BehaviorSubject<UserModel>(
+    this.user
+  );
+
+  readonly currentUser$ = this.currentUserSource.asObservable();
+
+  constructor() {}
+
+  getUser(): UserModel {
+    return this.currentUserSource.getValue();
+  }
+
+  setUser(user: UserModel): void {
+    this.currentUserSource.next(user);
+  }
+
+  removeUser(): void {
+    this.currentUserSource.next(this.user);
+  }
+}
---
+++ system/client/client/src/app/services/data/user/userModel.ts
@@ -0,0 +1,7 @@
+export interface UserModel {
+    id: string,
+    role: string,
+    firstName: string,
+    lastName: string,
+    email: string
+}
