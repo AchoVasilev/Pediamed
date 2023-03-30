f9fcbc2b22698de383287fe267b0d18726eec299
Angel Vasilev <angavasilev@gmail.com>
2023-03-30 22:16:44 +0300

    get the auth

--- system/server/src/main/java/server/application/services/auth/models/LoginResponse.java
+++ system/server/src/main/java/server/application/services/auth/models/LoginResponse.java
@@ -2,13 +2,14 @@ package server.application.services.auth.models;
 
 import server.infrastructure.utils.TokenModel;
 
+import java.util.List;
 import java.util.UUID;
 
 public record LoginResponse (
     UUID id,
     String firstName,
     String lastName,
-    String role,
+    List<String> role,
     String email,
     TokenModel tokenModel
 ) {}
--- system/server/src/main/java/server/presentation/auth/AuthController.java
+++ system/server/src/main/java/server/presentation/auth/AuthController.java
@@ -1,11 +1,16 @@
 package server.presentation.auth;
 
+import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.authentication.Authenticator;
+import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.security.handlers.LoginHandler;
 import io.micronaut.security.rules.SecurityRule;
+import reactor.core.publisher.Flux;
 import server.application.services.auth.AuthService;
 import server.application.services.auth.models.LoginRequest;
 import server.application.services.auth.models.RegistrationRequest;
@@ -14,15 +19,24 @@ import server.application.services.auth.models.UserDto;
 import javax.validation.Valid;
 
 @Controller(value = "auth")
+@Secured(SecurityRule.IS_ANONYMOUS)
 public class AuthController {
     private final AuthService authService;
+    private final Authenticator authenticator;
+    private final LoginHandler loginHandler;
 
-    public AuthController(AuthService authService) {
+    public AuthController(AuthService authService, Authenticator authenticator, LoginHandler loginHandler) {
         this.authService = authService;
+        this.authenticator = authenticator;
+        this.loginHandler = loginHandler;
     }
 
     @Post("/login")
-    public HttpResponse<UserDto> login(@Body LoginRequest loginRequest) {
+    public HttpResponse<UserDto> login(@Body LoginRequest loginRequest, HttpRequest<?> httpRequest) {
+        var credentials = new UsernamePasswordCredentials(loginRequest.email(), loginRequest.password());
+        var result = Flux.from(authenticator.authenticate(httpRequest, credentials));
+        var s = result.map(authenticationResponse -> authenticationResponse.getAuthentication().orElseThrow());
+
         var user = this.authService.findByEmail(loginRequest.email());
         return HttpResponse.ok(user);
     }
@@ -35,18 +49,13 @@ public class AuthController {
     }
 
 //    private LoginResponse getLoginResponse(Authentication authentication, boolean persist) {
-//        var role = authentication.getAuthorities()
-//                .stream()
-//                .findFirst()
-//                .orElseThrow(() -> new EntityNotFoundException(ENTITY_NOT_FOUND)).toString();
-//
 //        var user = this.authService.findByEmail(authentication.getName());
 //
 //        return new LoginResponse(
 //                user.id(),
 //                user.firstName(),
 //                user.lastName(),
-//                role,
+//                user.roleNames(),
 //                user.email(),
 //                this.tokenService.generateToken(authentication, persist)
 //        );
